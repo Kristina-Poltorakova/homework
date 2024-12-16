3c5e6adb3e794d66e4fccbf4b2888187b17e8a33
Kristina-Poltorakova <poltorakova@bk.ru>
2024-12-16 19:34:54 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+from fake_math import fake_divide as f_div
+from true_math import true_divide as t_div
+print('result1 = ', f_div(69, 3))
+print('result2 = ', f_div(3, 0))
+print('result3 = ', t_div(49, 7))
+print('result4 = ', t_div(15, 0))
